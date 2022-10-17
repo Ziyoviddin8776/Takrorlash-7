4cff87fd5a7bb5d8a488ac6d85e9b967ad0eceff
Ziyoviddin8776 <ziyoviddin8776@gmail.com>
2022-10-17 17:44:26 +0500

    String va uning metodlari

---
+++ main.js
@@ -0,0 +1,30 @@
+let str="salom";
+console.log(str.repeat(10))
+let plastik="8600 1402 7124 7190";
+let newPlastik=plastik.slice(0,15)
+console.log(newPlastik)
+console.log(newPlastik.padEnd(19,"*"))
+
+const rest="Salom men ketdim"
+console.log(rest.indexOf("m"))
+console.log(rest.indexOf("m",6))
+console.log(rest.lastIndexOf("m"))
+console.log(rest.startsWith("m",6))
+console.log(rest.endsWith("m"))
+console.log(rest.slice(6,9))
+console.log(rest.substring(5,2))
+console.log(rest.substr(3,10))
+console.log(rest.replace("a","o"))
+console.log(rest.toUpperCase())
+console.log(rest.toLocaleLowerCase())
+console.log(rest.concat(" uyga"))
+console.log(rest.trim())
+console.log(rest.charAt(10))
+console.log(rest.length)
+const arr=["a","l","l","a"]
+console.log(arr.join(""))
+console.log(rest.split(" "))
+
+
+
+// console.log(rest)
